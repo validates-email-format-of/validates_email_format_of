e8d3e781d68a8cae74449cc3ffd13aaaa23a3f8d
alexdunae <alexdunae@90efb8ff-712c-0410-86fd-957447a6ad18>
2008-03-06 20:39:18 +0000

    Added support for quoted local parts Added length checks for domain and local parts Corrected escaped character support for RFC 3696 Errata Added :allow_blank option Added :unless option Updated docs for 1.2.1 release
    git-svn-id: http://code.dunae.ca/validates_email_format_of/trunk@18 90efb8ff-712c-0410-86fd-957447a6ad18

--- lib/validates_email_format_of.rb
+++ lib/validates_email_format_of.rb
@@ -1,15 +1,8 @@
 module ValidatesEmailFormatOf
-  Local_Part_Special_Chars = Regexp.escape('!#$%&\'*-/=?+-^_`{|}~')
-  
-  Regex = Regexp.new(
-    '^(
-        (([[:alnum:]' + Local_Part_Special_Chars + ']|\\\\[\x00-\xFF])+[\.\+]+)
-      )*
-      ([[:alnum:]' + Local_Part_Special_Chars + '+]|\\\\[\x00-\xFF])+
-    @
-    (((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$)',
-    Regexp::EXTENDED | Regexp::IGNORECASE
-  )
+  LocalPartSpecialChars = Regexp.escape('!#$%&\'*-/=?+-^_`{|}~')
+  LocalPartUnquoted = '(([[:alnum:]' + LocalPartSpecialChars + ']+[\.\+]+))*[[:alnum:]' + LocalPartSpecialChars + '+]+'
+  LocalPartQuoted = '\"(([[:alnum:]' + LocalPartSpecialChars + '\.\+]*|(\\\\[\x00-\xFF]))*)\"'
+  Regex = Regexp.new('^((' + LocalPartUnquoted + ')|(' + LocalPartQuoted + ')+)@(((\w+\-+)|(\w+\.))*\w{1,63}\.[a-z]{2,6}$)', Regexp::EXTENDED | Regexp::IGNORECASE)
 end
 
 module ActiveRecord
@@ -24,28 +17,35 @@ module ActiveRecord
       # Configuration options:
       # * <tt>message</tt> - A custom error message (default is: " does not appear to be a valid e-mail address")
       # * <tt>on</tt> - Specifies when this validation is active (default is :save, other options :create, :update)
-      # * <tt>allow_nil</tt> Allow nil values (default is false)
+      # * <tt>allow_nil</tt> - Allow nil values (default is false)
+      # * <tt>allow_blank</tt> - Allow blank values (default is false)
       # * <tt>if</tt> - Specifies a method, proc or string to call to determine if the validation should
       #   occur (e.g. :if => :allow_validation, or :if => Proc.new { |user| user.signup_step > 2 }).  The
       #   method, proc or string should return or evaluate to a true or false value.
+      # * <tt>unless</tt> - See <tt>:if</tt>
       def validates_email_format_of(*attr_names)
-        configuration = { :message => ' does not appear to be a valid e-mail address', 
+        options = { :message => ' does not appear to be a valid e-mail address', 
                           :on => :save, 
                           :allow_nil => false,
+                          :allow_blank => false,
                           :with => ValidatesEmailFormatOf::Regex }
 
-        # local part must be max 64 chars
-        # domain part must be max of 255 chars
+        options.update(attr_names.pop) if attr_names.last.is_a?(Hash)
 
-        configuration.update(attr_names.pop) if attr_names.last.is_a?(Hash)
-
-        validates_each(attr_names, configuration) do |record, attr_name, value|
-          next if value.nil? and configuration[:allow_nil]
-
+        validates_each(attr_names, options) do |record, attr_name, value|
           v = value.to_s
 
-          unless v =~ configuration[:with] and not v =~ /\.\./
-            record.errors.add(attr_name, configuration[:message])
+          # local part max is 64 chars, domain part max is 255 chars
+          # TODO: should this decode escaped entities before counting?
+          begin
+            domain, local = v.reverse.split('@', 2)
+          rescue
+            record.errors.add(attr_name, options[:message])
+            next
+          end
+
+          unless v =~ options[:with] and not v =~ /\.\./ and domain.length <= 255 and local.length <= 64
+            record.errors.add(attr_name, options[:message])
           end
         end
       end
