713104c1171c2f13b320e5e71afdcc5b30454315
Alex Dunae <alex@dunae.ca>
2008-03-06 20:39:18 +0000

    Added support for quoted local parts Added length checks for domain and local parts Corrected escaped character support for RFC 3696 Errata Added :allow_blank option Added :unless option Updated docs for 1.2.1 release

--- test/validates_email_format_of_test.rb
+++ test/validates_email_format_of_test.rb
@@ -10,7 +10,7 @@ class ValidatesEmailFormatOfTest < Test::Unit::TestCase
   
   def test_should_allow_valid_email_addresses
     ['valid@example.com',
-     'valid@test.example.com',
+     'Valid@test.example.com',
      'valid+valid123@test.example.com',
      'valid_valid123@test.example.com',
      'valid-valid+123@test.example.co.uk',
@@ -25,12 +25,12 @@ class ValidatesEmailFormatOfTest < Test::Unit::TestCase
      'valid@example.mobi',
      'valid@example.info',
      'valid-@example.com',
-# from RFC 3696, page 6
+  # from RFC 3696, page 6
      'customer/department=shipping@example.com',
      '$A12345@example.com',
      '!def!xyz%abc@example.com',
      '_somename@example.com',
-# apostrophes and accents
+  # apostrophes
      "test'test@example.com",
      ].each do |email|
       p = create_person(:email => email)
@@ -40,11 +40,11 @@ class ValidatesEmailFormatOfTest < Test::Unit::TestCase
 
   def test_should_not_allow_invalid_email_addresses
     ['invalid@example-com',
-# period can not start local part
+  # period can not start local part
      '.invalid@example.com',
-# period can not end local part
+  # period can not end local part
      'invalid.@example.com', 
-# period can not appear twice consecutively
+  # period can not appear twice consecutively in local part
      'invali..d@example.com',
      'invalid@example.com.',
      'invalid@example.com_',
@@ -60,24 +60,35 @@ class ValidatesEmailFormatOfTest < Test::Unit::TestCase
     end
   end
 
-  # from http://tools.ietf.org/html/rfc3696, page 5
-  def test_should_allow_escaped_characters
-    ['Fred\ Bloggs_@example.com',
-     'Abc\@def+@example.com',
-     'Joe.\\Blow@example.com'
+  # from http://www.rfc-editor.org/errata_search.php?rfc=3696
+  def test_should_allow_quoted_characters
+    ['"Abc\@def"@example.com',     
+     '"Fred\ Bloggs"@example.com',
+     '"Joe.\\Blow"@example.com',
      ].each do |email|
       p = create_person(:email => email)
       save_passes(p, email)
     end
   end
 
-  def test_should_allow_quoted_characters
-    flunk
-    ['"Fred Bloggs"@example.com',     
-     '"Abc@def"@example.com'
+  # from http://tools.ietf.org/html/rfc3696, page 5
+  # corrected in http://www.rfc-editor.org/errata_search.php?rfc=3696
+  def test_should_not_allow_escaped_characters_without_quotes
+    ['Fred\ Bloggs_@example.com',
+     'Abc\@def+@example.com',
+     'Joe.\\Blow@example.com'
      ].each do |email|
       p = create_person(:email => email)
-      save_passes(p, email)
+      save_fails(p, email)
+    end
+  end
+
+  def test_should_check_length_limits
+    ['aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@example.com',
+     'test@aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.com'
+     ].each do |email|
+      p = create_person(:email => email)
+      save_fails(p, email)
     end
   end
 
@@ -85,6 +96,7 @@ class ValidatesEmailFormatOfTest < Test::Unit::TestCase
     p = create_person(:email => @valid_email)
     save_passes(p)
     
+    # we only asked to validate on :create so this should fail
     assert p.update_attributes(:email => @invalid_email)
     assert_equal @invalid_email, p.email
   end
