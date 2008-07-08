d5f5e487c5ace7f1942a2563ada1012a7229633a
Josh Nichols <josh@technicalpickles.com>
2008-07-07 20:40:56 -0400

    Removed active_support requires, which seem to make things unhappy when running rake test.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,10 +3,9 @@ RAILS_ROOT = File.dirname(__FILE__)
 
 require 'rubygems'
 require 'test/unit'
+require 'shoulda'
 require 'active_record'
 require 'active_record/fixtures'
-require 'active_support/binding_of_caller'
-require 'active_support/breakpoint'
 require "#{File.dirname(__FILE__)}/../init"
 
 
