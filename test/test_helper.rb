0c2d10d8ed7614e8a8e3ad877f6a790607ebcbca
Mike Burns <mburns@thoughtbot.com>
2008-12-23 18:02:41 -0500

    shoulda helper for the whole shebang

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -25,23 +25,23 @@ require "#{File.dirname(__FILE__)}/../init"
 
 class Test::Unit::TestCase #:nodoc:
   
-  def self.should_allow_values(*good_values)
+  def self.should_allow_values(klass,*good_values)
     get_options!(good_values)
     good_values.each do |v|
       should "allow email to be set to #{v.inspect}" do
-        user = User.new(:email => v)
+        user = klass.new(:email => v)
         user.save
         assert_nil user.errors.on(:email)
       end
     end
   end
 
-  def self.should_not_allow_values(*bad_values)
+  def self.should_not_allow_values(klass,*bad_values)
     message = get_options!(bad_values, :message)
     message ||= /invalid/
     bad_values.each do |v|
       should "not allow email to be set to #{v.inspect}" do
-        user = User.new(:email => v)
+        user = klass.new(:email => v)
         assert !user.save, "Saved user with email set to \"#{v}\""
         assert user.errors.on(:email), "There are no errors set on email after being set to \"#{v}\""
       end
--- test/validates_email_format_of_test.rb
+++ test/validates_email_format_of_test.rb
@@ -1,4 +1,5 @@
 require File.dirname(__FILE__) + '/test_helper'
+require File.dirname(__FILE__) + '/../shoulda_macros/validates_email_format_of'
 
 class User < ActiveRecord::Base
   validates_email_format_of :email, 
@@ -8,108 +9,5 @@ class User < ActiveRecord::Base
 end
 
 class ValidatesEmailFormatOfTest < Test::Unit::TestCase
-
-  context 'Typical valid email' do
-    should_allow_values  
-      ['valid@example.com',
-       'Valid@test.example.com',              
-       'valid+valid123@test.example.com',     
-       'valid_valid123@test.example.com',     
-       'valid-valid+123@test.example.co.uk',  
-       'valid-valid+1.23@test.example.com.au',
-       'valid@example.co.uk',                 
-       'v@example.com',                       
-       'valid@example.ca',                    
-       'valid_@example.com',                  
-       'valid123.456@example.org',            
-       'valid123.456@example.travel',         
-       'valid123.456@example.museum',         
-       'valid@example.mobi',                  
-       'valid@example.info',                  
-       'valid-@example.com']                 
-  end
-  
-  context 'valid email from RFC 3696, page 6' do
-    should_allow_values 
-      ['customer/department=shipping@example.com',
-       '$A12345@example.com',
-       '!def!xyz%abc@example.com',
-       '_somename@example.com']
-  end
-  
-  context 'valid email with apostrophe' do
-    should_allow_values "test'test@example.com"
-  end
-  
-  context 'valid email from http://www.rfc-editor.org/errata_search.php?rfc=3696' do
-    should_allow_values
-      ['"Abc\@def"@example.com',     
-       '"Fred\ Bloggs"@example.com',
-       '"Joe.\\Blow"@example.com']
-  end
-  
-  context 'Typical invalid email' do
-    should_not_allow_values
-      ['invalid@example-com',
-       'invalid@example.com.',
-       'invalid@example.com_',
-       'invalid@example.com-',
-       'invalid-example.com',
-       'invalid@example.b#r.com',
-       'invalid@example.c',
-       'invali d@example.com',
-       'invalidexample.com',
-       'invalid@example.']
-  end
-  
-  context 'invalid email with period starting local part' do
-    should_not_allow_values '.invalid@example.com'
-  end
-  
-  context 'invalid email with period ending local part' do
-    should_not_allow_values 'invalid.@example.com'
-  end
-  
-  context 'invalid email with consecutive periods' do
-    should_not_allow_values 'invali..d@example.com'
-  end
-  
-  # corrected in http://www.rfc-editor.org/errata_search.php?rfc=3696
-  context 'invalid email from http://tools.ietf.org/html/rfc3696, page 5' do
-    should_not_allow_values 
-      ['Fred\ Bloggs_@example.com',
-       'Abc\@def+@example.com',
-       'Joe.\\Blow@example.com']
-  end
-
-  context 'invalid email exceeding length limits' do
-    should_not_allow_values
-      ['aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa@example.com',
-       'test@aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.com']
-  end
-  
-  context 'An invalid user on update' do
-    setup do
-      @user = User.new(:email => 'dcroak@thoughtbot.com')
-      @user.save
-      @user.update_attribute :email, '..dcroak@thoughtbot.com'
-    end
-      
-    should 'pass validation' do
-      assert @user.valid?
-      assert @user.save
-      assert_nil @user.errors.on(:email)
-    end
-  end
-  
-  context 'A user with a nil email' do
-    setup { @user = User.new(:email => nil) }
-    
-    should 'pass validation' do
-      assert @user.valid?
-      assert @user.save
-      assert_nil @user.errors.on(:email)
-    end
-  end
-
+  should_validate_email_format_of_klass(User, :email)
 end
