a2d2119c5f3588dc5d303a07b51504f6904e2256
Richard Hirner <richard@hirner.at>
2010-02-20 23:38:44 +0100

    Allow A records as a fallback, too

--- lib/validates_email_format_of.rb
+++ lib/validates_email_format_of.rb
@@ -9,7 +9,7 @@ module ValidatesEmailFormatOf
   def self.validate_email_domain(email)
     domain = email.match(/\@(.+)/)[1]
     Resolv::DNS.open do |dns|
-      @mx = dns.getresources(domain, Resolv::DNS::Resource::IN::MX)
+      @mx = dns.getresources(domain, Resolv::DNS::Resource::IN::MX) + dns.getresources(domain, Resolv::DNS::Resource::IN::A)
     end
     @mx.size > 0 ? true : false
   end
