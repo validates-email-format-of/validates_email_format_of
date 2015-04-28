5dbe36aa4fadc872129c09d7234169cb52b8fa99
Kent Nagata <ngtknt@me.com>
2015-04-28 16:00:22 +0900

    fixed #46.

--- lib/validates_email_format_of/rspec_matcher.rb
+++ lib/validates_email_format_of/rspec_matcher.rb
@@ -4,9 +4,8 @@ RSpec::Matchers.define :validate_email_format_of do |attribute|
   match do
     actual = subject.is_a?(Class) ? subject.new : subject
     actual.send(:"#{attribute}=", "invalid@example.")
-    expect(actual).to be_invalid
     @expected_message ||= ValidatesEmailFormatOf.default_message
-    expect(actual.errors.messages[attribute.to_sym]).to include(@expected_message)
+    !actual.valid? && actual.errors.messages[attribute.to_sym].include?(@expected_message)
   end
   chain :with_message do |message|
     @expected_message = message
