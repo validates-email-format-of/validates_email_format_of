868c8ca1776b69f2bf52406f8956c84ed60b2a60
NhuLuu <nhu@tribex.co>
2022-08-03 16:56:51 +0700

    Fix rspec run fail when using custom error message

--- lib/validates_email_format_of/rspec_matcher.rb
+++ lib/validates_email_format_of/rspec_matcher.rb
@@ -6,7 +6,7 @@ RSpec::Matchers.define :validate_email_format_of do |attribute|
     actual.send(:"#{attribute}=", "invalid@example.")
     expect(actual).to be_invalid
     @expected_message ||= ValidatesEmailFormatOf.default_message
-    expect(actual.errors.added?(attribute, :invalid_email_address)).to be_truthy
+    expect(actual.errors.added?(attribute, @expected_message)).to be_truthy
   end
   chain :with_message do |message|
     @expected_message = message
