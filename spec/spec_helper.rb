0467b3fb131d21c58605e83ba8ab1fc0f99ee141
Alex Dunae <alex@dunae.ca>
2024-03-09 19:01:47 -0800

    Improve specs to allow inspecting the subject model
    Instead of just looking at errors.full_messages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,9 @@ require "pry"
 require "byebug"
 
 RSpec::Matchers.define :have_errors_on_email do
-  match do |actual|
+  match do |user|
+    actual = user.errors.full_messages
+    expect(user.errors.added?(:email, ValidatesEmailFormatOf::ERROR_MESSAGE_I18N_KEY))
     expect(actual).not_to be_nil, "#{actual} should not be nil"
     expect(actual).not_to be_empty, "#{actual} should not be empty"
     expect(actual.size).to eq(@reasons.size), "#{actual} should have #{@reasons.size} elements"
@@ -19,7 +21,7 @@ RSpec::Matchers.define :have_errors_on_email do
   chain :and_because do |reason|
     (@reasons ||= []) << reason
   end
-  match_when_negated do |actual|
-    expect(actual).to(be_empty)
+  match_when_negated do |user|
+    expect(user.errors).to(be_empty)
   end
 end
